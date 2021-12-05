f081bec2ad71384b3a1bfd71cc33089e3a8c83ec
Jorbon <ben.a.phillips@outlook.com>
2021-12-04 21:50:36 -0600

    initial

---
+++ src/main/java/edu/jorbonism/unbound_elytra/UnboundElytra.java
@@ -0,0 +1,10 @@
+package edu.jorbonism.unbound_elytra;
+
+import net.fabricmc.api.ModInitializer;
+
+public class UnboundElytra implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        
+    }
+}
